692a78d277295413bfdb89c1330cb5db4fa998ab
tchivs <tchivs@DESKTOP-76EKLS8>
2020-03-14 18:27:49 +0800

    add reggion

--- Tchivs.Ioc/ApplicationBase.cs
+++ Tchivs.Ioc/ApplicationBase.cs
@@ -3,17 +3,34 @@ namespace Tchivs.Ioc
 {
     public abstract class ApplicationBase
     {
-        protected ITcLog Logger { get; set; }
+        #region properties
+
+        protected ITcLog Logger { get; }
+
+        #endregion
+
+        #region constructors
 
         protected ApplicationBase(ITcLogProvider logProvider)
         {
             Logger = logProvider.GetLogFor<ApplicationBase>();
         }
+
+        #endregion
+
+        #region abstracct methods
+
         /// <summary>
         /// 启动应用程序
         /// </summary>
         /// <param name="arguments">参数</param>
         public abstract void Start(string[] arguments);
+
+        /// <summary>
+        /// 停止应用程序
+        /// </summary>
         public abstract void Stop();
+
+        #endregion
     }
 }
